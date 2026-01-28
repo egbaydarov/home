b7e592417644b45663ae2e36cebc4e62b4819f2e
egbaydarov <eg@byda.io>
2026-01-28 02:40:04 +0100

    better tmux exp

--- .config/nvim/lua/byda/lazy/oil.lua
+++ .config/nvim/lua/byda/lazy/oil.lua
@@ -13,6 +13,7 @@ return {
         default_file_explorer = true,
         columns = {
           "icon",
+          "permissions",
         },
         -- Buffer-local options to use for oil buffers
         buf_options = {
@@ -31,7 +32,7 @@ return {
           concealcursor = "nvic",
         },
         -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-        delete_to_trash = false,
+        delete_to_trash = true,
         -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
         skip_confirm_for_simple_edits = false,
         -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
@@ -72,7 +73,7 @@ return {
           ["<C-l>"] = "actions.refresh",
           ["-"] = { "actions.parent", mode = "n" },
           ["_"] = { "actions.open_cwd", mode = "n" },
-          ["`"] = { "actions.cd", mode = "n" },
+          ["<leader>."] = { "actions.cd", mode = "n" },
           ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
           ["gs"] = { "actions.change_sort", mode = "n" },
           ["gx"] = "actions.open_external",
--- .config/tmux/vim-visual.sh
+++ .config/tmux/vim-visual.sh
@@ -12,35 +12,20 @@ IS_VIM=0
 if [ "$PANE_CMD" = "vim" ] || [ "$PANE_CMD" = "nvim" ]; then
   IS_VIM=1
   # write buffer + view (cursor/scroll) without changing the view
-  tmux send-keys ":silent! keepalt keepjumps w! $TMPFILE | call writefile([json_encode(winsaveview())], '$TMPVIEW')" Enter
 
-  # wait until files exist (tmux send-keys is async)
-  i=0
-  while [ $i -lt 200 ] && { [ ! -s "$TMPFILE" ] || [ ! -s "$TMPVIEW" ]; }; do
-    i=$((i+1))
-    sleep 0.001
-  done
+  ACK="$(mktemp -u).fifo"
+  mkfifo "$ACK"
+  tmux send-keys ":silent! keepalt keepjumps w! $TMPFILE | call writefile([json_encode(winsaveview())], '$TMPVIEW') | call writefile(['ok'], '$ACK') | redraw! | redrawstatus!" Enter
+  read _ < "$ACK"
+  rm -f "$ACK"
+
 else
   tmux capture-pane -J -pS - > "$TMPFILE"
   : > "$TMPVIEW"  # empty marker; no view to restore
 fi
 
-# Trim trailing blank lines top, bottom, and right
-awk '
-{
-  sub(/[[:space:]]+$/, "", $0)
-  if (!started) {
-    if ($0 ~ /^[[:space:]]*$/) next
-    started = 1
-  }
-  if ($0 ~ /^[[:space:]]*$/) {
-    blank = blank $0 ORS
-    next
-  }
-  if (blank) { printf "%s", blank; blank = "" }
-  print
-}
-' "$TMPFILE" > "$TMPFILE.trim" && mv "$TMPFILE.trim" "$TMPFILE"
+# Trim trailing blank lines bottom, and right
+sed -i ':a;/^[[:space:]]*$/{$d;N;ba};$!{$!s/[[:space:]]\+$//}' "$TMPFILE"
 
 WINDOW_ID="$(tmux new-window -P -d "$VIM_COMMAND -R \
   -c 'syntax on' \
@@ -48,12 +33,22 @@ WINDOW_ID="$(tmux new-window -P -d "$VIM_COMMAND -R \
   -c 'syntax match TmuxPrompt /^\[[^]]\+\][#$]/' \
   -c 'hi def link TmuxPrompt Special' \
   -c 'nnoremap <silent><buffer> q :quit<CR>' \
-  -c 'set nomodifiable laststatus=0 noshowcmd noruler noshowmode cmdheight=0' \
-  -c \"if $IS_VIM | set number norelativenumber | else | set nonumber norelativenumber | endif\" \
-  -c \"if filereadable('$TMPVIEW') && getfsize('$TMPVIEW') > 0 | let v=json_decode(join(readfile('$TMPVIEW'), '')) | call winrestview(v) | else | norm! G$ | endif\" \
+  -c 'set nomodifiable noshowcmd noruler noshowmode cmdheight=0' \
+  -c 'setlocal statusline=%=%#StatusLine#\\ COPY\\ MODE\\ %=' \
+  -c \"if $IS_VIM |
+        set number norelativenumber signcolumn=yes foldcolumn=0 |
+      else |
+        set nonumber norelativenumber signcolumn=no foldcolumn=0 |
+      endif\" \
+  -c \"if filereadable('$TMPVIEW') && getfsize('$TMPVIEW') > 0 |
+        let v=json_decode(join(readfile('$TMPVIEW'), '')) |
+        call winrestview(v) |
+      else |
+        norm! G$ |
+      endif\" \
   $TMPFILE")"
 
-tmux rename-window -t "$WINDOW_ID" copy
+tmux rename-window -t "$WINDOW_ID" copy-mode
 tmux select-window -t "$WINDOW_ID"
 tmux set-window-option -t "$WINDOW_ID" remain-on-exit off
 
